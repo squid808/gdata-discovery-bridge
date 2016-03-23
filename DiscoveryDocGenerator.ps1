3c5dbd07a02220ae12760aa0f05bf639d999167e
Spencer Varney <squid808@gmail.com>
2016-03-23 01:06:18 -0400

    Initial commit of generator and admin_emailsettings_v1 source file

---
+++ DiscoveryDocGenerator.ps1
@@ -0,0 +1,387 @@
+﻿try {
+    [DocCreation.ResourceType] -eq $null | Out-Null
+} catch {
+    $MyEnums = @"
+namespace DocCreation
+{
+    //[System.FlagsAttribute]
+    public enum ResourceType
+    { delete, get, insert, list, patch, update }
+
+    public enum HttpMethod
+    { GET, POST, PUT, DELETE, INSERT }
+
+    public enum ParameterType
+    { query, path }
+
+    public enum SchemaPropType
+    { @string, boolean, integer, array, @object, any }
+
+    public enum SchemaPropFormat
+    { @byte, date, datetime, @double, int32, int64, uint32 }
+}
+"@
+
+    Add-Type -TypeDefinition $MyEnums -Language CSharpVersion3
+
+    Remove-Variable MyEnums
+}
+
+function New-DiscoveryDocument{
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$ApiName,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$ApiVersion,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$CanonicalName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Title,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$Description,
+
+    [Parameter(Mandatory=$true)]
+    [string]$DocLink,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$BaseUrl,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$BasePath
+)
+    if ($BaseUrl[-1] -ne "/") {$BaseUrl += "/"}
+    if ($BasePath[0] -eq "/") {$BasePath = $BasePath.TrimStart("/")}
+    if ($BasePath[-1] -ne "/") {$BasePath += "/"}
+
+    $ParamsObj = [ordered]@{
+        alt = [ordered]@{
+            type = "string"
+            description = "Data format for the response."
+            default = "atom+xml"
+            enum = @("atom+xml")
+            enumDescriptions = @("Responses with Content-Type of application/xml")
+            location = "query"
+        }
+        fields = [ordered]@{
+            type = "string"
+            description = "Selector specifying which fields to include in a partial response."
+            location = "query"
+        }
+        key = [ordered]@{
+            type = "string"
+            description = "API key. Your API key identifies your project and provides you with API access, quota, and reports. Required unless you provide an OAuth 2.0 token."
+            location = "query"
+        }
+        oauth_token = [ordered]@{
+            type = "string"
+            description = "Oauth 2.0 token for the current user."
+            location = "query"
+        }
+        prettyPrint = [ordered]@{
+            type = "boolean"
+            description = "Returns response with indentations and line breaks."
+            default = "true"
+            location = "query"
+        }
+        quotaUser = [ordered]@{
+            type = "string"
+            description = "Available to use for quota purposes for server-side applications. Can be any arbitrary string assigned to a user, but should not exceed 40 characters. Overrides userIp if both are provided."
+            location = "query"
+        }
+        userIp = [ordered]@{
+            type = "string"
+            description = "IP address of the site where the request originates. Use this if you want to enforce per-user limits."
+            location = "query"
+        }
+    }
+
+    $JsonObj = [ordered]@{
+        isGdata = $true.ToString().ToLower()
+        kind = "discovery#restDescription"
+        etag = ""
+        discoveryVersion = "https://github.com/squid808/"
+        id = $ApiName + ":" + $ApiVersion
+        name = $ApiName
+        canonicalName = $CanonicalName
+        version = $ApiVersion
+        revision = (Get-Date).ToString("YYYYmmDD")
+        title = $Title
+        description = $Description
+        ownerDomain = "google.com"
+        ownerName = "Google"
+        packagePath = $ApiName
+        icons = @{}
+        documentationLink = $DocLink
+        protocol = "rest"
+        baseUrl = $BaseUrl + "/" + $BasePath
+        basePath = "/" + $BasePath
+        rootUrl = $BaseUrl
+        servicePath = $BasePath
+        batchPath = "batch"
+        parameters = $ParamsObj
+        auth = [ordered]@{
+            oauth2 = [ordered]@{
+                scopes = [ordered]@{}
+            }
+        }
+        schemas = [ordered]@{}
+        resources = [ordered]@{}
+    }
+
+    return (New-Object psobject -Property $JsonObj)
+}
+
+function Add-Scope {
+Param(
+    [Parameter(Mandatory=$true)]
+    [psobject]$JsonObj,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Scope,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Description
+)
+    $JsonObj.auth.oauth2.scopes[$Scope] = [ordered]@{description = $Description}
+}
+
+function Add-Resource {
+Param(
+    [Parameter(Mandatory=$true)]
+    [psobject]$JsonObj,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Resource
+)
+
+    if ($JsonObj.resources.Contains($Resource)) {
+        Write-Error "Resource already exists."
+    } else {
+        $JsonObj.resources[$Resource] = [ordered]@{
+            methods = [ordered]@{}
+        }
+    }
+
+}
+
+function Add-Method {
+Param(
+    [Parameter(Mandatory=$true)]
+    [psobject]$JsonObj,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$Resource,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$IdPrefix,
+
+    [Parameter(Mandatory=$true)]
+    [DocCreation.ResourceType]$MethodName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Path,
+    
+    [Parameter(Mandatory=$true)]
+    [DocCreation.HttpMethod]$HttpMethod,
+    
+    [Parameter(Mandatory=$true)]
+    [System.Collections.ICollection]$Scopes,
+
+
+    [string]$Description="",
+    [string]$RequestObjRef=$null,
+    [string]$ResponseObjRef=$null
+)
+
+    [string]$MethodName = $MethodName.ToString()
+    [string]$HttpMethod = $HttpMethod.ToString()
+
+    if (-not $JsonObj.resources.Contains($Resource)) {
+        $JsonObj.resources[$Resource] = [ordered]@{methods=[ordered]@{}}
+    }
+
+    $MethodObj = [ordered]@{
+        id = "$IdPrefix.$Resource.$MethodName"
+        path = $Path
+        httpMethod = $HttpMethod
+        description = $Description
+    }
+
+    if ($RequestObjRef) {$MethodObj.Add("request",@{"`$ref"=$RequestObjRef})}
+
+    $MethodObj.Add("parameters", [ordered]@{})
+
+    if ($ResponseObjRef) {$MethodObj.Add("response",@{"`$ref"=$ResponseObjRef})}
+
+    $MethodObj.Add("scopes", $Scopes)
+
+    $JsonObj.resources[$Resource].methods[$MethodName] = $MethodObj
+}
+
+function Add-MethodParam {
+Param(
+    [Parameter(Mandatory=$true)]
+    [psobject]$JsonObj,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Resource,
+
+    [Parameter(Mandatory=$true)]
+    [DocCreation.ResourceType]$Method,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Type,
+
+    [Parameter(Mandatory=$true)]
+    [string]$ParamName,    
+
+    [Parameter(Mandatory=$true)]
+    [bool]$IsRequired,
+
+    [Parameter(Mandatory=$true)]
+    [DocCreation.ParameterType]$Location,
+
+    [int]$ParamOrder,
+    [string]$Description = ""
+
+)
+
+    [string]$Method = $Method.ToString()
+    [string]$Location = $Location.ToString()
+
+    if ($JsonObj.resources.Contains($Resource)) {
+
+        if ($JsonObj.resources[$Resource].methods.Contains($Method)){
+
+            $ParamObj = [ordered]@{
+                type = $Type
+                description = $Description
+                required = $IsRequired.ToString().ToLower()
+                location = $Location
+            }
+
+            $JsonObj.resources[$Resource].methods[$Method].parameters[$ParamName] = $ParamObj
+
+            if ($paramOrder) {
+                if (-not ($JsonObj.resources[$Resource].methods[$Method].Contains("parameterOrder"))){
+                    $JsonObj.resources[$Resource].methods[$Method].Add("parameterOrder", (New-Object System.Collections.ArrayList))
+                }
+
+                $JsonObj.resources[$Resource].methods[$Method].parameterOrder.Insert($ParamOrder-1, $ParamName)
+            }
+
+        } else {
+            Write-Error "Cannot find the Method on the Json Object Resource."
+        }
+    } else {
+        Write-Error "Cannot find the Resource on the Json Object."
+    }
+
+}
+
+function Add-Schema {
+Param(
+    [Parameter(Mandatory=$true)]
+    [psobject]$JsonObj,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Schema,
+
+    [string]$Description = ""
+)
+
+    $SchemaObj = [ordered]@{
+        id = $Schema
+        type = "object"
+        description = $Description
+        properties = [ordered]@{}
+    }
+
+    if ($JsonObj.schemas.Contains($Schema)) {
+        Write-Error "Schema already exists."
+    } else {
+        $JsonObj.schemas[$Schema] = $SchemaObj
+    }
+}
+
+function Add-SchemaProperty {
+Param(
+    [Parameter(Mandatory=$true)]
+    [psobject]$JsonObj,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Schema,
+    
+    [Parameter(Mandatory=$true)]
+    [DocCreation.SchemaPropType]$Type,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Property,
+
+    [string]$Description="",
+
+    [DocCreation.SchemaPropFormat]$Format,
+
+    [string]$ArrayObjRef
+)
+
+    [string]$Type = $Type.ToString()
+
+    if ($Format -eq [DocCreation.SchemaPropFormat]::datetime){
+        [string]$Format = "date-time"
+    } elseif ($Format -ne $null) {
+        [string]$Format = $Format.ToString();
+    }
+
+    if ($JsonObj.schemas.Contains($Schema)) {
+        if ($JsonObj.schemas[$Schema].properties.Contains($Property)){
+            Write-Error "Schema property already exists."
+        } else {
+            $PropertyObj = [ordered]@{
+                type = $Type
+                description = $Description
+            }
+
+            if ($Type -eq "array") {
+                $PropertyObj["items"] = [ordered]@{
+                    "`$ref" = $ArrayObjRef
+                }
+            }
+
+            if ($Format) {$PropertyObj["format"] = $Format}
+
+            $JsonObj.schemas[$Schema].properties[$Property] = $PropertyObj
+        }
+    } else {
+        Write-Error "Schema doesn't exist."
+    }
+}
+
+function Export-DiscoveryDoc {
+Param(
+    [Parameter(Mandatory=$true)]
+    [psobject]$JsonObj,
+
+    [Parameter(Mandatory=$true)]
+    [int]$ReductionFactor
+)
+
+    $J = $JsonObj | ConvertTo-Json -Depth 15
+
+    $lines = ($JsonObj | ConvertTo-Json -Depth 15) -split '\r\n'
+
+    for ($i = 0; $i -lt $lines.Length; $i ++){
+    #foreach ($line in $Lines) {
+        if ($lines[$i] -match '^ +') {
+            $lines[$i] = ' ' * ($Matches[0].Length / $ReductionFactor) + $lines[$i].TrimStart()
+        }
+    }
+
+    $J = ($Lines -join "`r`n") -replace '{( |\r\n)+}', '{}'
+
+    return $J
+}
---
+++ admin_emailsettings_v1.ps1
@@ -0,0 +1,278 @@
+﻿. .\DiscoveryDocGenerator.ps1 
+
+$Json = New-DiscoveryDocument `
+    -ApiName "admin"`
+    -ApiVersion "emailsettings_v1"`
+    -CanonicalName "emailsettings"`
+    -Title "Admin Email Settings API"`
+    -Description "Enables Google partners to programmatically manipulate most user-level Google Mail settings."`
+    -DocLink "https://developers.google.com/admin-sdk/reports/"`
+    -BaseUrl "https://apps-apis.google.com/"`
+    -BasePath "a/feeds/emailsettings/2.0/"
+
+Add-Scope -JsonObj $Json `
+    -Scope "https://apps-apis.google.com/a/feeds/emailsettings/2.0/" `
+    -Description "Global scope for access to all email settings"
+
+
+#SCHEMA - use the XML Attribute Name here
+$SchemaName = "Delegate"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName string address
+Add-SchemaProperty $Json $SchemaName string delegate
+Add-SchemaProperty $Json $SchemaName string delegationId
+Add-SchemaProperty $Json $SchemaName string status
+
+$SchemaName = "Delegates"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName array delegates -ArrayObjRef Delegate
+
+$SchemaName = "Filter"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName string from
+Add-SchemaProperty $Json $SchemaName string to
+Add-SchemaProperty $Json $SchemaName string subject
+Add-SchemaProperty $Json $SchemaName string hasTheWord
+Add-SchemaProperty $Json $SchemaName string doesNotHaveTheWord
+Add-SchemaProperty $Json $SchemaName boolean hasAttachment
+
+Add-SchemaProperty $Json $SchemaName boolean shouldArchive
+Add-SchemaProperty $Json $SchemaName boolean shouldMarkAsRead
+Add-SchemaProperty $Json $SchemaName boolean shouldStar
+Add-SchemaProperty $Json $SchemaName string label
+Add-SchemaProperty $Json $SchemaName string forwardTo
+Add-SchemaProperty $Json $SchemaName boolean shouldTrash
+Add-SchemaProperty $Json $SchemaName boolean neverSpam
+
+$SchemaName = "Forwarding"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName boolean enable
+Add-SchemaProperty $Json $SchemaName string forwardTo
+Add-SchemaProperty $Json $SchemaName string action #KEEP, ARCHIVE, DELETE, MARK_READ
+
+$SchemaName = "General"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName integer pageSize #25, 50, or 100
+Add-SchemaProperty $Json $SchemaName boolean shortcuts
+Add-SchemaProperty $Json $SchemaName boolean arrows
+Add-SchemaProperty $Json $SchemaName boolean snippets
+Add-SchemaProperty $Json $SchemaName boolean unicode
+
+$SchemaName = "Imap"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName boolean enable
+
+$SchemaName = "Label"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName string label
+Add-SchemaProperty $Json $SchemaName string labelid
+Add-SchemaProperty $Json $SchemaName string unreadCount -Format uint32
+Add-SchemaProperty $Json $SchemaName string visibility
+
+$SchemaName = "Labels"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName array labels -ArrayObjRef Label
+
+$SchemaName = "Language"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName string language #LIST IS ON WEBSITE
+
+$SchemaName = "Pop"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName boolean enable
+Add-SchemaProperty $Json $SchemaName string enableFor # ALL_MAIL or MAIL_FROM_NOW_ON
+Add-SchemaProperty $Json $SchemaName string action #KEEP, ARCHIVE or DELETE
+
+$SchemaName = "SendasAlias"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName string name
+Add-SchemaProperty $Json $SchemaName string address
+Add-SchemaProperty $Json $SchemaName string replyTo
+Add-SchemaProperty $Json $SchemaName boolean makeDefault
+
+$SchemaName = "SendAsAliases"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName array sendasAliases -ArrayObjRef SendasAlias
+
+$SchemaName = "Signature"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName string signature
+
+$SchemaName = "VacationResponder"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName boolean contactsOnly
+Add-SchemaProperty $Json $SchemaName boolean domainOnly
+Add-SchemaProperty $Json $SchemaName boolean enable
+Add-SchemaProperty $Json $SchemaName string endDate -Format date
+Add-SchemaProperty $Json $SchemaName string message
+Add-SchemaProperty $Json $SchemaName string startDate -Format date
+Add-SchemaProperty $Json $SchemaName string subject
+
+$SchemaName = "WebClip"
+Add-Schema $Json $SchemaName
+Add-SchemaProperty $Json $SchemaName boolean enable
+
+
+##METHODS
+#DELEGATION
+$MethodName = "delegation"
+$MethodType = "delete"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/delegation/{delegateEmail}" DELETE `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/")
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+Add-MethodParam $Json $MethodName $MethodType string delegateEmail $true path -ParamOrder 3
+
+$MethodType = "get"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/delegation" GET `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Delegates
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+$MethodType = "insert"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/delegation" POST `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Delegate -RequestObjRef Delegate
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#FILTERS
+$MethodName = "filters"
+$MethodType = "insert"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/filter" POST `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -RequestObjRef Filter -ResponseObjRef Filter
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#FORWARDING
+$MethodName = "forwarding"
+$MethodType = "get"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/forwarding" GET `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Forwarding
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+$MethodType = "update"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/forwarding" PUT `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -RequestObjRef Forwarding -ResponseObjRef Forwarding
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#GENERAL
+$MethodName = "general"
+$MethodType = "update"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/general" PUT `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -RequestObjRef General -ResponseObjRef General
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#IMAP
+$MethodName = "imap"
+$MethodType = "get"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/imap" GET `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Imap
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+$MethodType = "update"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/imap" PUT `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Imap -RequestObjRef Imap
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#LABELS
+$MethodName = "labels"
+$MethodType = "delete"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/label/{labelName}" DELETE `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/")
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+Add-MethodParam $Json $MethodName $MethodType string labelName $true path -ParamOrder 3
+
+$MethodType = "get"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/label" GET `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Labels
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+$MethodType = "insert"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/label" POST `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Label -RequestObjRef Label
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#LANGUAGE
+$MethodName = "language"
+$MethodType = "update"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/language" PUT `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Language -RequestObjRef Language
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#POP
+$MethodName = "pop"
+$MethodType = "get"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/pop" GET `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Pop
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+$MethodType = "update"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/pop" PUT `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Pop -RequestObjRef Pop
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#SIGNATURE
+$MethodName = "signature"
+$MethodType = "get"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/signature" GET `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Signature
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+$MethodType = "update"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/signature" PUT `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef Signature -RequestObjRef Signature
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#SENDAS ALIAS
+$MethodName = "sendasAliases"
+$MethodType = "get"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/sendas" GET `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef SendAsAliases
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+$MethodType = "insert"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/sendas" POST `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef SendasAlias -RequestObjRef SendasAlias
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#VACATION RESPONDER
+$MethodName = "vacationResponder"
+$MethodType = "get"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/vacation" GET `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef VacationResponder
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+$MethodType = "update"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/vacation" PUT `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef VacationResponder -RequestObjRef VacationResponder
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#WEBCLIPS
+$MethodName = "webClip"
+$MethodType = "update"
+Add-Method $Json $MethodName emailsettings $MethodType "{domain}/{userKey}/webclip" PUT `
+    -Scopes @("https://apps-apis.google.com/a/feeds/emailsettings/2.0/") -ResponseObjRef WebClip -RequestObjRef WebClip
+Add-MethodParam $Json $MethodName $MethodType string domain $true path -ParamOrder 1
+Add-MethodParam $Json $MethodName $MethodType string userKey $true path -ParamOrder 2
+
+#FINAL
+$SourceFolder = Split-Path -parent $PSCommandPath
+$DestFile = $SourceFolder + "\admin_emailsettings_v1.json"
+Export-DiscoveryDoc $Json -ReductionFactor 16 | Out-File -FilePath $DestFile
